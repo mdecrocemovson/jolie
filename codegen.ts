4364c4b2d7fad92c60d9b8fc967c64cf1c7f52a5
Michael DeCroce-Movson <mdecrocemovson@gmail.com>
2023-09-04 16:44:30 -0400

    adding nocheck to generated

---
+++ codegen.ts
@@ -0,0 +1,37 @@
+const config = {
+  overwrite: true,
+  schema: "http://localhost:8080/graphql",
+  documents: "./graphql/schemas/*.graphql",
+  generates: {
+    "./lib/generated/politics-service.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-query",
+        {
+          add: {
+            content: "// @ts-nocheck",
+          },
+        },
+      ],
+      config: {
+        fetcher: "graphql-request",
+        exposeDocument: true,
+        exposeQueryKeys: true,
+        exposeFetcher: true,
+        scalars: {
+          DateTime: "Date",
+          Date: "Date",
+          Time: "Date",
+          JSON: "{ [key: string]: any }",
+          BigDecimal: "number",
+          NonNegativeInt: "number",
+          PositiveInt: "number",
+        },
+        addInfiniteQuery: false,
+      },
+    },
+  },
+};
+
+export default config;
