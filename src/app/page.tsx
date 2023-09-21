5b8093887982960f3c7979fa7804f286e2221a43
Michael DeCroce-Movson <mdecrocemovson@gmail.com>
2023-09-21 15:25:10 -0700

    with cannoli

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import { Typography } from "@mui/material";
+import Image from "next/image";
 
 const Politician = () => {
   return (
-    <Typography variant="h1" component="h2">
-      Hello betches
-    </Typography>
+    <>
+      <Typography variant="h1" component="h2">
+        Hello betches
+      </Typography>
+      <Image
+        alt="Pic of cannoli"
+        width={600}
+        height={800}
+        src="/cannoli.jpeg"
+      />
+    </>
   );
 };
 
